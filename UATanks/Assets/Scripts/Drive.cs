b7a61bb3f6c9dd100b1a32c48ae53c264edb27f1
Jerard Carney <jercarne@uat.edu>
2019-05-09 15:56:54 -0600

    Tank Creation
    Added Drive, NEED to make "s" have it's own reverse speed. 
    
    Added Turret swivel Mechanic.
    
    NEEDS... Bullets to shoot, Shoot Mechanic, Enemies, Need to figure out how to individualize the WASD

---
+++ UATanks/Assets/Scripts/Drive.cs
@@ -0,0 +1,27 @@
+﻿// Drive 
+
+// This is the drive mechanic for the player tank
+
+// Unity Libraries
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class... Drive
+public class Drive : MonoBehaviour
+{
+    // Variables... tank movespeed, and turret move speed.
+    public float moveSpeed = 1;
+    public float rotationSpeed = 12;
+
+    void FixedUpdate()
+    {
+        //This is the location of tank 
+        float moveVector = Input.GetAxis("Vertical");
+        float rotateVector = Input.GetAxis("Horizontal");
+
+        // Mechanic... This is the equation. Location x move speed x time rate.
+        this.transform.Translate(0f, moveVector * moveSpeed * Time.deltaTime, 0f);
+        this.transform.Rotate(0f, 0f, -rotateVector * (rotationSpeed* 10) * Time.deltaTime);
+    }
+}
