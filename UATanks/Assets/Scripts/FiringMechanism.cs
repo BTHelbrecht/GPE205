0c64ca49a3662504856c4e427cb1374246946a88
Jerard Carney <jercarne@uat.edu>
2019-05-09 17:20:50 -0600

    Shooting Mechanic
    Started shooting Mechanic. Debug PASS
    
    Next is to put bullet in to the game with mechanic.

---
+++ UATanks/Assets/Scripts/FiringMechanism.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiringMechanism : MonoBehaviour
+{
+    public float fireRate = 3;
+    public float damage = 15;
+    float timeUntilFire = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > timeUntilFire)
+        {
+            timeUntilFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Debug.Log("Shooting");
+    }
+}
