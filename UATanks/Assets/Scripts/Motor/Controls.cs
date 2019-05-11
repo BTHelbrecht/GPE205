b31ee317444fb8af825248838e72106eb2e89c5c
Jerard Carney <jercarne@uat.edu>
2019-05-10 22:34:11 -0600

    Motor
    Took back the scope and revamped the basics now I need to get this working code into the Forward class and have it reference correctly.

--- UATanks/Assets/Scripts/Motor/Controls.cs
+++ UATanks/Assets/Scripts/Motor/Controls.cs
@@ -4,47 +4,48 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
+    public float speedForward;
+    public float speedReverse;
+
     public Rigidbody2D body;
 
-    [SerializeField] private Forward forward;
-    [SerializeField] private Reverse reverse;
-    [SerializeField] private Rotate rotate;
-   
-    
-     void Start()
+
+    void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        forward = GameObject.FindGameObjectWithTag("Motor").GetComponent<Forward>();
-        reverse = GameObject.FindGameObjectWithTag("Motor").GetComponent<Reverse>();
-        rotate = GameObject.FindGameObjectWithTag("Motor").GetComponent<Rotate>();
     }
 
     void Update()
+    {
+        Move();
+    }
+
+    public void Move()
     {
         if (Input.GetButton("w"))
         {
             //move forward
             Debug.Log("f");
-            forward.MoveForward();
-
+            transform.Translate(Vector2.up * speedForward * Time.deltaTime);
         }
         if (Input.GetButton("s"))
         {
             //move reverse
             Debug.Log("r");
-            reverse.MoveReverse();
+            transform.Translate(Vector2.down * speedReverse * Time.deltaTime);
         }
         if (Input.GetButton("a"))
         {
             //rotate left
             Debug.Log("RL");
-            rotate.MoveRotate();
+
         }
         if (Input.GetButton("d"))
         {
             //rotate right
             Debug.Log("RR");
-            rotate.MoveRotate();
+
         }
     }
 }
+
--- UATanks/Assets/Scripts/Motor/Forward.cs
+++ UATanks/Assets/Scripts/Motor/Forward.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class Forward : MonoBehaviour
 {
-    public float speedForward;
-
-    public Rigidbody2D body;
-
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
-    }
-
-    public void MoveForward()
-    {
-        Debug.Log("Forawrd");
-        Vector2 forward = new Vector2(0, Input.GetAxis("w"));
-    }
-
-    void FixedUpdate()
-    {
-        body.MovePosition(body.position * (speedForward * Time.fixedDeltaTime));
-    }
+    
 }
