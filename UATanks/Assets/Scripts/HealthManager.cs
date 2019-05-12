e3659efa640e004d8538fc6cb45cb76f2df0bc2b
Jerard Carney <jercarne@uat.edu>
2019-05-12 14:12:55 -0600

    Project
    Quick Save

--- UATanks/Assets/Scripts/HealthManager.cs
+++ UATanks/Assets/Scripts/HealthManager.cs
@@ -5,23 +5,13 @@ using UnityEngine;
 public class HealthManager : MonoBehaviour
 {
     public int health = 3;
-    public float invulnerableTimer = 0;
-    private int correctLayer;
-
-    void Start()
-    {
-        correctLayer = gameObject.layer;
-    }
 
     
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("TRIG");
-
-        
         health--;
-        invulnerableTimer = 2f;
-        gameObject.layer = 10;
         
     }
 
@@ -29,19 +19,12 @@ public class HealthManager : MonoBehaviour
 
     void Update()
     {
-        invulnerableTimer -= Time.deltaTime;
-
-        if (invulnerableTimer <= 0)
-        {
-            gameObject.layer = correctLayer;
-        }
-
         if (health <= 0)
         {
             Remove();
         }
     }
-
+    
 
 
     void Remove()
