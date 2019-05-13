5ef63bd0aa2e338f983467534dd52c05842d9c0f
Jerard Carney <jercarne@uat.edu>
2019-05-12 19:39:51 -0600

    Enemies
    Updated the Health Manager
    
    Need UI

--- UATanks/Assets/Scripts/Damage/HealthManager.cs
+++ UATanks/Assets/Scripts/Damage/HealthManager.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    public int health = 3;
-    
+    public int health = 5;
+    public int bulletDamage = 2;
+    public int ramDamage = 1;
     
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("TRIG");
-        health--;
-        
+        if(collision.gameObject.tag == "Enemy")
+        {
+            health -= ramDamage;
+            Debug.Log("enemy");
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            health -= ramDamage;
+            Debug.Log("player");
+        }
+
+        if (collision.gameObject.tag == "Bullet")
+        {
+            health -= bulletDamage;
+            Debug.Log("bullet");
+        }
     }
 
 
