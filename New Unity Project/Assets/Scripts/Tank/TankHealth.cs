7ac96244d7a33e63acefdfd0962127e09046d2d4
Jerard Carney <jercarne@uat.edu>
2019-05-19 16:55:00 -0600

    Health UI
    Added:
    .Canvas
    .Health Script
    .Slider
    ..Background
    ..Fill
    This is a health wheel to see health as a player plays the game.
    
    NEED:
    New enemy prefab and AI  and Turret Update and Bullet.

--- New Unity Project/Assets/Scripts/Tank/TankHealth.cs
+++ New Unity Project/Assets/Scripts/Tank/TankHealth.cs
@@ -3,20 +3,21 @@ using UnityEngine.UI;
 
 public class TankHealth : MonoBehaviour
 {
-    public float m_StartingHealth = 100f;          
-    public Slider m_Slider;                        
-    public Image m_FillImage;                      
-    public Color m_FullHealthColor = Color.green;  
-    public Color m_ZeroHealthColor = Color.red;    
-    public GameObject m_ExplosionPrefab;
+    public float m_StartingHealth = 100f;           // Starting total of health = 100
+    public Slider m_Slider;                         // Variable to reference values of the slider
+    public Image m_FillImage;                       // To acces the fill gameobject
+    public Color m_FullHealthColor = Color.green;   // green is for full health on the UI wheel
+    public Color m_ZeroHealthColor = Color.red;     // red is for no health on the UI wheel
+   // public GameObject m_ExplosionPrefab;            // refrence to the explosion prefab
     
     /*
     private AudioSource m_ExplosionAudio;          
-    private ParticleSystem m_ExplosionParticles;   
-    private float m_CurrentHealth;  
-    private bool m_Dead;            
+    private ParticleSystem m_ExplosionParticles;  
+    */
+    private float m_CurrentHealth;                  // value for the current health
+    private bool m_Dead;                            // value to check if player is dead or not
 
-
+    /*
     private void Awake()
     {
         m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
@@ -24,6 +25,7 @@ public class TankHealth : MonoBehaviour
 
         m_ExplosionParticles.gameObject.SetActive(false);
     }
+    */
 
 
     private void OnEnable()
@@ -33,22 +35,37 @@ public class TankHealth : MonoBehaviour
 
         SetHealthUI();
     }
-    */
+    
 
     public void TakeDamage(float amount)
     {
         // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        m_CurrentHealth -= amount;
+
+        SetHealthUI();
+
+        if(m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
     }
 
 
     private void SetHealthUI()
     {
         // Adjust the value and colour of the slider.
+        m_Slider.value = m_CurrentHealth;
+
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
     }
 
 
     private void OnDeath()
     {
         // Play the effects for the death of the tank and deactivate it.
+        m_Dead = true;
+
+        gameObject.SetActive(false);
+
     }
 }
